63ad61fe317ccc5a886290ef13540d47e73b0fbe
Vlad Eliseev <freshymanchan@gmail.com>
2020-01-07 00:54:08 +1000

    Add functions for further algorithm changes

---
+++ src/services/blocksService.js
@@ -0,0 +1,104 @@
+import parse from 'json-to-ast';
+
+function convertTreeToList(root) {
+  var stack = [], array = [];
+  stack.push(root);
+
+  while(stack.length !== 0) {
+      var node = stack.pop();
+      array.push(node);
+
+      if(!node.content) {
+          continue;
+      } else {
+          for (var i = node.content.length - 1; i >= 0; i--) {
+              stack.push(node.content[i]);
+          }
+      }
+  }
+
+  return array;
+}
+
+function isBlock(node) {
+  const childrenKeys = node.children.map((child) => {
+    return child.key.value;
+  })
+
+  if (childrenKeys.includes('block')) {
+    return true;
+  }
+
+  return false;
+}
+
+function getChildrenOf(node) {
+  const contentProperty = node.children.find((child) => {
+    return child.key.value === 'content'
+  })
+
+  if (!contentProperty) {
+    return []
+  }
+
+  return contentProperty.value.children
+}
+
+function convertAstTreeToList(root) {
+  var stack = [], array = [];
+  stack.push(root);
+
+  while(stack.length !== 0) {
+      var node = stack.pop();
+
+      if (isBlock(node)) {
+        array.push(node);
+      }
+
+      const nodeChildren = getChildrenOf(node);
+
+      if (nodeChildren.length === 0) {
+        continue;
+      } else {
+        for (var i = nodeChildren.length - 1; i >= 0; i--) {
+          stack.push(nodeChildren[i]);
+        }
+      }
+  }
+
+  return array;
+}
+
+function getBlocksWithLocation(blocks, astBlocks) {
+  const blockWithLocation = blocks.map((block, index) => {
+    let result = block;
+
+    const astBlock = astBlocks[index];
+    const astChildren = getChildrenOf(astBlock);
+
+    if (block.content) {
+      const contentWithLoc = result.content.map((children, index) => {
+        const {loc} = astChildren[index];
+        return {...block, loc}
+      })
+
+      result.content = contentWithLoc;
+    }
+
+    const {loc} = astBlock;
+    return {...result, loc}
+  });
+
+  return blockWithLocation;
+}
+
+export function getBlocks(json) {
+  const blocksList = convertTreeToList(json);
+
+  const ast = parse(jsonString);
+  const astBlocksList = convertAstTreeToList(ast);
+
+  const blocksWithLocation = getBlocksWithLocation(blocksList, astBlocksList);
+  
+  return blocksWithLocation;
+}
