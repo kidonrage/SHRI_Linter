90cec09e050b8d4611364c4faa1d767b2f92b362
Vlad Eliseev <freshymanchan@gmail.com>
2020-01-07 21:55:09 +1000

    Improve tests flexibility

--- tests/warning/textSize.js
+++ tests/warning/textSize.js
@@ -1,24 +1,56 @@
 import chai from 'chai';
 import lint from '../../src/index';
 
-const warningTextDifference = {
-  failure: `{
-    "block": "warning",
-    "content": [
-        { "block": "text", "mods": { "size": "l" } },
-        { "block": "text", "mods": { "size": "m" } }
-    ]
-  }`,
-  success: `{
-    "block": "warning",
-    "content": [
-        { "block": "text", "mods": { "size": "l" } },
-        { "block": "text", "mods": { "size": "l" } }
-    ]
-  }`
-}
+const testInputs = [
+  { 
+    json: `{
+      "block": "warning",
+      "content": [
+        {
+          "block": "placeholder",
+          "mods": { "size": "m" }
+        },
+        {
+          "elem": "content",
+          "content": [
+            {
+                "block": "text",
+                "mods": { "size": "m" }
+            },
+            {
+                "block": "text",
+                "mods": { "size": "l" }
+            }
+          ]
+        }
+      ]
+    }`,
+    expectedErrors: 1
+  },
+  {
+    json: `{
+      "block": "warning",
+      "content": [
+          { "block": "text", "mods": { "size": "l" } },
+          { "block": "text", "mods": { "size": "m" } }
+      ]
+    }`,
+    expectedErrors: 1
+  },
+  {
+    json: `{
+      "block": "warning",
+      "content": [
+          { "block": "text", "mods": { "size": "l" } },
+          { "block": "text", "mods": { "size": "l" } }
+      ]
+    }`,
+    expectedErrors: 0
+  }
+]
 
 export default function textSize() {
-  chai.assert.equal(lint(warningTextDifference.failure).length, 1);
-  chai.assert.equal(lint(warningTextDifference.success).length, 0);
+  testInputs.forEach((input) => {
+    chai.assert.equal(lint(input.json).length, input.expectedErrors);
+  });
 };
