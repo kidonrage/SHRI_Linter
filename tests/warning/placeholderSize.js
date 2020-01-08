1f031784fef7d4c2f24dd8f08547a9c64cf3104f
Vlad Eliseev <freshymanchan@gmail.com>
2020-01-08 16:48:37 +1000

    Fix tests errors

--- tests/warning/placeholderSize.js
+++ tests/warning/placeholderSize.js
@@ -6,7 +6,7 @@ const testInputs = [
     json: `{
       "block": "warning",
       "content": [
-        { "block": "placeholder", "mods": { "size": "xl" }
+        { "block": "placeholder", "mods": { "size": "xl" }}
       ]
     }`,
     expectedErrors: 1
@@ -15,14 +15,14 @@ const testInputs = [
     json: `{
       "block": "warning",
       "content": [
-        { "block": "placeholder", "mods": { "size": "m" }
+        { "block": "placeholder", "mods": { "size": "m" }}
       ]
     }`,
     expectedErrors: 0
   }
 ]
 
-export default function buttonSize() {
+export default function placeholderSize() {
   testInputs.forEach((input) => {
     const result = lint(input.json);
     chai.assert.equal(result.length, input.expectedErrors);
