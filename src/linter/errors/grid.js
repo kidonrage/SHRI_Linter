ad4681be7c46e171c40d649d4805e0dc3a559e74
Vlad Eliseev <freshymanchan@gmail.com>
2020-01-10 15:18:33 +1000

    Add new errors for grid blocks

---
+++ src/linter/errors/grid.js
@@ -0,0 +1,8 @@
+const grid = {
+  advertisements: {
+    code: 'GRID.TOO_MUCH_MARKETING_BLOCKS',
+    desc: 'Маркетинговые блоки занимают больше половины от всех колонок блока grid.'
+  }
+}
+
+export default grid;
